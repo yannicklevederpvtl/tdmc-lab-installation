eb20a17319dbfa4461387ec55fbfd5f5dcdcd8a7
yannicklevederpvtl <56557352+yannicklevederpvtl@users.noreply.github.com>
2025-02-25 17:30:03 -0500

    Update download.sh

--- download.sh
+++ download.sh
@@ -38,7 +38,7 @@ if [ ! -f /usr/local/bin/ytt ]; then
 fi
 
 if [ ! -f /bin/pandoc ]; then
-  echo "Installing YTT"
+  echo "Installing Pandoc"
   wget -q https://github.com/jgm/pandoc/releases/download/3.6.3/pandoc-3.6.3-1-amd64.deb
   sudo sudo dpkg -i ./pandoc-3.6.3-1-amd64.deb
   rm -f ./pandoc-3.6.3-1-amd64.deb
